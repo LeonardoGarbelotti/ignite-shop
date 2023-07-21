f4b4e98238c9e35f39b88632eb814d683ba56d4b
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2023-07-21 10:12:24 -0300

    Stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,78 @@
-export default function Home() {
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import { stripe } from "@/lib/stripe";
+
+import { useKeenSlider } from 'keen-slider/react'
+import "keen-slider/keen-slider.min.css"
+
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Stripe from "stripe";
+
+interface IHomeProps {
+  products: {
+    id: string
+    name: string
+    image_url: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }: IHomeProps) {
+  const [sliderRef] = useKeenSlider({
+    mode: 'snap',
+    slides: {
+      perView: 'auto',
+      spacing: 48,
+    }
+  })
+
   return (
-    <>
-      <h1>hellow</h1>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image
+              src={product.image_url}
+              width={520}
+              height={480}
+              alt={product.name}
+            />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+    </HomeContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      image_url: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format((price.unit_amount! / 100)),
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // re-valida a página a cada 2 horas
+  }
 }
